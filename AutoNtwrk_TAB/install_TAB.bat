dc1f81931155ce22ed1817e7f6d21857d5a15698
Chris McLernon <c.r.mclernon@gmail.com>
2025-05-13 08:31:50 -0400

    Changes to be committed: 	modified:   AutoNtwrk_TAB/install_TAB.bat

--- AutoNtwrk_TAB/install_TAB.bat
+++ AutoNtwrk_TAB/install_TAB.bat
@@ -7,8 +7,8 @@ set SCRIPT_PATH=%~dp0%SCRIPT_NAME%
 
 echo Installing Task Scheduler task for SSID Checker...
 
-:: Create the task to run at logon
-schtasks /Create /TN "%TASK_NAME%" /TR "powershell -ExecutionPolicy Bypass -File \"%SCRIPT_PATH%\"" /SC ONLOGON /RL HIGHEST /F
+:: Create the task to run every 15 mins
+schtasks /Create /TN "%TASK_NAME%" /TR "powershell -ExecutionPolicy Bypass -File \"%SCRIPT_PATH%\"" /SC MINUTE /MO 15 /RL HIGHEST /F
 
 echo.
 echo Installed. The script will now run at user logon.
